84ab7fed444f1fa128bb96bfd73e34d750744324
Moshe <mb-dev@users.noreply.github.com>
2019-01-25 13:38:54 -0800

    updates

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -8,6 +8,20 @@ local display_monitor = "DELL U2415"
 
 local screen_watcher = nil
 
+local units = {
+  right30       = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+  right70       = { x = 0.30, y = 0.00, w = 0.70, h = 1.00 },
+  left70        = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
+  left30        = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
+  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  upright30     = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
+  botright30    = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
+  upleft70      = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
+  botleft70     = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
+  maximum       = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 }
+}
+
 ----------------------
 -- System Callbacks --
 ----------------------
@@ -41,8 +55,10 @@ end
 
 hs.hotkey.bind(mash, ',', function() os.execute("open -n -a /Applications/Utilities/Terminal.app/") end)
 
-hs.hotkey.bind(mash, 'Left', function() hs.window.focusedWindow():moveToUnit(hs.layout.left50) end)
-hs.hotkey.bind(mash, 'Right', function() hs.window.focusedWindow():moveToUnit(hs.layout.right50) end)
+hs.hotkey.bind(mash, 'Left', function() hs.window.focusedWindow():moveToUnit(units.left50) end)
+hs.hotkey.bind(mash, 'Right', function() hs.window.focusedWindow():moveToUnit(units.right50) end)
+hs.hotkey.bind(mash, 'Up', function() hs.window.focusedWindow():moveToUnit(units.top50) end)
+hs.hotkey.bind(mash, 'Down', function() hs.window.focusedWindow():moveToUnit(units.bot50) end)
 hs.hotkey.bind(mash, 'M', hs.grid.maximizeWindow)
 
 hs.hotkey.bind(mash, ',', function()
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,4 +9,5 @@ ln -s ~/workspace/dotfiles/.config/vim ~/.config/vim
 ln -s ~/workspace/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/workspace/dotfiles/.vim/init.vim ~/.vimrc
 ln -s ~/workspace/dotfiles/.hammerspoon ~/.hammerspoon
-ln -s ~/vscode-settings.json /Users/moshebe/Library/Application\ Support/Code/User/settings.json
+ln -s ~/workspace/dotfiles/.vscode/settings.json /Users/moshebe/Library/Application\ Support/Code/User/settings.json
+ln -s ~/workspace/dotfiles/.vscode/keybindings.json /Users/moshebe/Library/Application\ Support/Code/User/keybindings.json
